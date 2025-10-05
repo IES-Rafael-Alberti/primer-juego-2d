c50300c72a7cf0fca87972c42985c72678e88b89
JesCorAvi <jesuscorderoavila1@gmail.com>
2025-10-05 21:38:04 +0200

    añadidas puntuaciones

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,15 +1,8 @@
 extends Node
 
+@onready var game_manager: Node = %GameManager
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 
 func _on_body_entered(body: Node2D):
-	print("+1 coin") # Replace with function body.
+	game_manager.add_point()
 	queue_free()

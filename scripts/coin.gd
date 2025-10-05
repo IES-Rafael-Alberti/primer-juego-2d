1311866c0f11d577d992cc5c61694ab24b22bc62
JesCorAvi <jesuscorderoavila1@gmail.com>
2025-10-05 21:51:34 +0200

    añadidos los sonidos

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,9 @@
 extends Node
 
 @onready var game_manager: Node = %GameManager
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
 
 
 func _on_body_entered(body: Node2D):
 	game_manager.add_point()
-	queue_free()
+	animation_player.play("pickup")
